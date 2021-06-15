68b02ed6790ff46428b1eb463fd5cc0ca078760e
luizasilveira <luizars7@hotmail.com>
2021-06-15 05:16:46 +0000

    paralelismo

--- material/aulas/14-exercicios/pi_recursivo.cpp
+++ material/aulas/14-exercicios/pi_recursivo.cpp
@@ -1,25 +1,56 @@
 #include <omp.h>
 #include <iostream>
 #include <iomanip>
-static long num_steps = 1024l*1024*1024*2;
+static long num_steps = 1024l*1024*1024*4;
 
 #define MIN_BLK  1024*1024*256
 
 double sum = 0;
 
-void pi_r(long Nstart, long Nfinish, double step) {
+// void pi_r(long Nstart, long Nfinish, double step) {
+//     long i,iblk;
+//     if (Nfinish-Nstart < MIN_BLK){
+//         //#pragma omp parallel for reduction(+:sum)
+//         for (i = Nstart; i < Nfinish; i++){
+//             double x = (i+0.5)*step;
+//             sum += 4.0/(1.0+x*x); 
+//         }
+//     } else {
+//         iblk = Nfinish-Nstart;
+//         pi_r(Nstart,         Nfinish-iblk/2,step);
+//         pi_r(Nfinish-iblk/2, Nfinish,       step);
+//     }
+// }
+
+double pi_r(long Nstart, long Nfinish, double step) {
     long i,iblk;
+    double sum = 0;
     if (Nfinish-Nstart < MIN_BLK){
+        //#pragma omp parallel for reduction(+:sum)
         for (i = Nstart; i < Nfinish; i++){
             double x = (i+0.5)*step;
             sum += 4.0/(1.0+x*x); 
         }
     } else {
         iblk = Nfinish-Nstart;
-        pi_r(Nstart,         Nfinish-iblk/2,step);
-        pi_r(Nfinish-iblk/2, Nfinish,       step);
+        double sum1, sum2;
+        #pragma omp parallel
+        {
+            #pragma omp master
+            {
+                
+                //Nao pode acessar o mesmo dado em duas threads diferentes
+                #pragma omp task shared(sum1)
+                    sum1 = pi_r(Nstart,         Nfinish-iblk/2,step);
+                #pragma omp task shared(sum2)
+                    sum2 = pi_r(Nfinish-iblk/2, Nfinish,       step);
+            }       
+        }
+        sum = sum1 + sum2;
     }
+    return sum;
 }
+
 
 int main () {
     long i;
@@ -27,7 +58,7 @@ int main () {
     double init_time, final_time;
     step = 1.0/(double) num_steps;
     init_time = omp_get_wtime();
-    pi_r(0, num_steps, step);
+    double sum = pi_r(0, num_steps, step);
     pi = step * sum;
     final_time = omp_get_wtime() - init_time;
 
--- material/aulas/15-efeitos-colaterais/vetor_insert.cpp
+++ material/aulas/15-efeitos-colaterais/vetor_insert.cpp
@@ -4,18 +4,25 @@
 
 
 double conta_complexa(int i) {
+	sleep(1);
 	return 2 * i;
 }
 
 int main() {
-	int N = 10000; 
+	int N = 10; 
 	std::vector<double> vec;
+	#pragma omp parallel for default(none) shared(vec) firstprivate(N)
 	for (int i = 0; i < N; i++) {
-		vec.push_back(conta_complexa(i));
+		double p = conta_complexa(i);
+		#pragma omp critical
+		{
+			vec.push_back(p);
+		}
+		
 	}
 	
 	for (int i = 0; i < N; i++) {
-		std::cout << i << " ";
+		std::cout << vec[i] << " ";
 	}
 	
 	return 0;
